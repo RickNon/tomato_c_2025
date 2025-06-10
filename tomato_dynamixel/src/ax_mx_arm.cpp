6955415cfe245262e96bf6b8df8a7e8a4632ae98
MukaiMasao215 <mukai_masao@stu.kobe-u.ac.jp>
2025-06-10 16:23:00 +0900

    #5 支持座標に制限を設けた

--- tomato_dynamixel/src/ax_mx_arm.cpp
+++ tomato_dynamixel/src/ax_mx_arm.cpp
@@ -209,8 +209,13 @@ int main(int argc, char** argv) {
     }
 
     // Update IK target and compute new positions
-    target_point.point.x += cmd_x;
-    target_point.point.y += cmd_y;
+    if (27556 > target_point.point.x*target_point.point.x + target_point.point.y*target_point.point.y){
+      target_point.point.x += cmd_x;
+      target_point.point.y += cmd_y;  
+    }else{
+      target_point.point.x -= cmd_x;
+      target_point.point.y -= cmd_y;
+    }
     InverseAngles inv_res = inversed_kinematics(target_point.point.x, target_point.point.y, M_PI/2);
     position_ax[1] = int(inv_res.A_angle_2 / M_PI / 2 * 1024 + 512);
     position_ax[2] = int(inv_res.A_angle_3 / M_PI / 2 * 1024 + 512);
