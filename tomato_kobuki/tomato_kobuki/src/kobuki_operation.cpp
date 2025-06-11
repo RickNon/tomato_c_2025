a3619fb71483da72470186c1dede042814de8012
RickNon <nonomura_rikuto@stu.kobe-u.ac.jp>
2025-06-11 15:49:26 +0900

    #9 kobuki遠隔操縦の加減速制御を修正

--- tomato_kobuki/tomato_kobuki/src/kobuki_operation.cpp
+++ tomato_kobuki/tomato_kobuki/src/kobuki_operation.cpp
@@ -28,9 +28,7 @@ void KobukiOperation::joy_callback(const sensor_msgs::Joy &joy_msg)
 {
     double _g_turn = 1;
     double rotation_v_tmp = joy_msg.axes[0] * _g_turn;
-    
-    // 加速度制限の無視
-     _no_acc_limit = (std::abs(joy_msg.axes[0]) <= 0.5 && std::abs(joy_msg.axes[1]) <= 0.5);
+
     motion_v_tmp = joy_msg.axes[1] * _g_speed;
     if (joy_msg.axes[1] < 0){
         rotation_v_tmp = - joy_msg.axes[0] * _g_turn;
@@ -110,30 +108,18 @@ void KobukiOperation::kobukiInterpolate()
     double speed_diff = _control.target_speed - _control.control_speed;
     double turn_diff = _control.target_turn - _control.control_turn;
 
+    // 加速度制限の無視
+    _no_acc_limit = (std::abs(speed_diff) <= 0.06 && std::abs(turn_diff) <= 0.3);
+
     if (_no_acc_limit) {
         // 加速度制限なしで即時反映
         _control.control_speed = _control.target_speed;
         _control.control_turn = _control.target_turn;
     } 
     else {
-          // 線形速度の補間（加速度制限）
-        if (std::abs(speed_diff) < _speed_acc) {
-            _control.control_speed = _control.target_speed;
-            //  std::cout << "No" << std::endl;
-        } 
-        else {
-            _control.control_speed += (_speed_acc * (speed_diff > 0 ? 1 : -1));
-            //std::cout << "OKaaaaaaay" << std::endl;
-        }
-          // 角速度の補間（加速度制限）
- 
-        if (std::abs(turn_diff) < _turn_acc) {
-            _control.control_turn = _control.target_turn;
-        } 
-        else {
-            _control.control_turn += (_turn_acc * (turn_diff > 0 ? 1 : -1));
-        }
-
+        // 線形速度の補間（加速度制限）
+        _control.control_speed += (_speed_acc * (speed_diff > 0 ? 1 : -1));
+        _control.control_turn += (_turn_acc * (turn_diff > 0 ? 1 : -1));
     }
     geometry_msgs::Twist command;
     command.linear.x = _control.control_speed;
